d3b8fb8d981b835c0d20df68005691a08a14f0eb
Mikhail Tsyplakov <jaybekster@gmail.com>
2019-12-11 14:56:02 +0000

    hw-4: done

--- hw-4/main.go
+++ hw-4/main.go
@@ -4,23 +4,48 @@ import (
 	"strings"
 	"fmt"
 	"unicode"
+	"sort"
 )
 
-func freqentWords(input string) map[string]int {
-	output := make(map[string]int)
+func freqentWords(input string) []string {
+	type WordsArrayItem struct {
+		name string
+		total int
+	}
+
+	counter := make(map[string]int)
+	wordsArray := []WordsArrayItem{}
+	output := make([]string, 0)
 
 	for _, word := range strings.FieldsFunc(input, func(c rune) bool {
 		return !unicode.IsLetter(c)
 	}) {
 		key := strings.ToLower(word)
-		output[key] = output[key] + 1;
+		counter[key] = counter[key] + 1;
+	}
+
+	for k, v := range counter {
+		wordsArray = append(wordsArray, WordsArrayItem{ k, v })
+	}
+
+	sort.SliceStable(wordsArray, func(i, j int) bool {
+		return wordsArray[i].total > wordsArray[j].total
+	})
+
+	for i := 0; i < 10; i++ {
+		if len(wordsArray) <= i {
+			break;
+		}
+		output = append(output, wordsArray[i].name)
 	}
 
 	return output
 }
 
 func main() {
-	fmt.Println(freqentWords("hello word hello"))
+	fmt.Println(freqentWords("hello world hello"))
 
-	fmt.Println(freqentWords("Hello word, hellO!"))
+	fmt.Println(freqentWords("Hello world, hellO!"))
+
+	fmt.Println(freqentWords("ops oops oops oops oops asd asd, hellO!"))
 }
