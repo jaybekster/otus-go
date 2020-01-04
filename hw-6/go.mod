bcce86ad31fd0606c4f7f9f720f83d8b8cf65cf1
Mikhail Tsyplakov <jaybekster@gmail.com>
2020-01-04 15:50:41 +0000

    hw-6: wip

--- hw-6/go.mod
+++ hw-6/go.mod
@@ -1,3 +1,5 @@
 module github.com/jaybekster/otus-go/hw-6
 
 go 1.13
+
+require github.com/cheggaaa/pb/v3 v3.0.3 // indirect
--- hw-6/main.go
+++ hw-6/main.go
@@ -1,5 +1,57 @@
 package main
 
-func main() {
+import (
+	"flag"
+	"fmt"
+	"io"
+	"os"
+	"log"
+)
 
+var from, to string
+var offset, limit int64
+
+
+func init() {
+	flag.StringVar(&from, "from", "a.txt", "original file")
+	flag.StringVar(&to, "to", "b.txt", "destionation file file")
+	flag.Int64Var(&offset, "offset", 0, "offset in original file")
+	flag.Int64Var(&limit, "limit", 1024, "number of copying bytes")
+}
+
+func main() {
+	flag.Parse()
+
+	Copy()
+}
+
+func Copy() {
+	fileRead, err := os.Open(from)
+
+	defer fileRead.Close()
+
+	if err != nil {
+		log.Fatalf("Can not open source file")
+	}
+
+	fileWrite, err := os.Open(to)
+
+	if err != nil {
+		log.Fatalf("Can not open destination file")
+	}
+
+	defer fileWrite.Close()
+
+	fileRead.Seek(offset, 0)
+
+	_, error := io.CopyN(fileWrite, fileRead, limit)
+
+	if error == io.EOF {
+		log.Printf("%s", "Copying is finished")
+	} else if error != nil {
+		fmt.Println(error)
+		log.Fatalf("%s", "An error occured in copying proccess")
+	}
+
+	fmt.Println(err)
 }
