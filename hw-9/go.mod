a7ddcce3784391d32618a50681c1f7cd79435c40
Mikhail Tsyplakov <jaybekster@gmail.com>
2020-02-12 15:14:16 +0000

    hw-9: pass pah to config file; parse config file via viper;

--- hw-9/go.mod
+++ hw-9/go.mod
@@ -6,6 +6,7 @@ require (
 	github.com/pelletier/go-toml v1.6.0 // indirect
 	github.com/spf13/afero v1.2.2 // indirect
 	github.com/spf13/cast v1.3.1 // indirect
+	github.com/spf13/flag v0.0.0-20140426202619-916eaf9e2dc5
 	github.com/spf13/jwalterweatherman v1.1.0 // indirect
 	github.com/spf13/pflag v1.0.5
 	github.com/spf13/viper v1.6.2
--- hw-9/main.go
+++ hw-9/main.go
@@ -1,18 +1,29 @@
 package main
 
 import (
-	"github.com/jaybekster/otus-go/hw-9/config"
-	"github.com/spf13/viper"
 	"log"
+
+	"github.com/jaybekster/otus-go/hw-9/config"
+	pflag "github.com/spf13/flag"
+	"github.com/spf13/viper"
 )
 
 func main() {
-	viper.SetConfigName("config")
-	viper.AddConfigPath(".")
+	var configPath *string = pflag.String("config", "./config.yml", "path to config file")
+
+	pflag.Parse()
+
+	viper.SetConfigFile(*configPath)
 
 	var configuration config.Configuration
 
 	if err := viper.ReadInConfig(); err != nil {
 		log.Fatalf("Error reading config file, %s", err)
 	}
+	err := viper.Unmarshal(&configuration)
+	if err != nil {
+		log.Fatalf("unable to decode into struct, %v", err)
+	}
+
+	log.Println(configuration)
 }
