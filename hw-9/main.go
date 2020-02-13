7d7b28b97c3f7d4f6042b244239f8d41838f8909
Mikhail Tsyplakov <jaybekster@gmail.com>
2020-02-13 08:32:53 +0000

    hw-9: server and /hello endpoint

--- hw-9/main.go
+++ hw-9/main.go
@@ -1,7 +1,11 @@
 package main
 
 import (
+	"fmt"
 	"log"
+	"net/http"
+	"strconv"
+	"strings"
 
 	"github.com/jaybekster/otus-go/hw-9/config"
 	pflag "github.com/spf13/flag"
@@ -25,5 +29,20 @@ func main() {
 		log.Fatalf("unable to decode into struct, %v", err)
 	}
 
-	log.Println(configuration)
+	http.HandleFunc("/hello", helloController)
+
+	err = http.ListenAndServe(
+		strings.Join([]string{configuration.Http_listen.Ip, strconv.Itoa(configuration.Http_listen.Port)}, ":"),
+		nil,
+	)
+
+	log.Printf("Server start lisening on port: %d", configuration.Http_listen.Port)
+
+	if err != nil {
+		log.Fatalf("server can not start, %v", err)
+	}
+}
+
+func helloController(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "Hello world!")
 }
